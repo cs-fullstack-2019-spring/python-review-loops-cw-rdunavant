2868be67122f292161247f1370e59d5278cdfc3c
rdunavant0 <rdunavant0@gmail.com>
2019-01-22 11:50:18 -0600

    message

---
+++ Ex1.py
@@ -0,0 +1,40 @@
+#Exercise1
+#Write Python program that prints all the numbers from 0 to 6 except 3 and 6.
+def exercise1():
+    for num in range(0,6):
+        if (num == 3 or num == 6):
+            continue
+        print(num)
+    # (0<==6)
+    # but (3,6)
+    # print(0,1,2,4,5)
+
+#Exercise2
+#Write Python program to count number of even and odd numbers from series of numbers.
+def exercise2():
+    for num in range(1,2,3,4,5,6,7,8,9):
+.even (numbers)
+.odd (numbers)
+(.even(numbers) + .odd(numbers))
+
+#Exercise3
+#Write Python program that accepts sequence of lines as input.
+#Print the lines as output after User enters blank line to end.
+def exercise3():
+    for lines in sequence(sequence of lines):
+blankLine = input "(Enter blank line)";
+print(lines)
+
+#Bonus
+#Write Python program to construct pattern.
+#Use nested for loop.
+def bonus():
+    for asteriks in pattern(*
+                            **
+                            ***
+                            ****
+                            *****
+                            ****
+                            ***
+                            **
+                            *);
